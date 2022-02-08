04c78a64fda4cff138f44ac0462ddadab25c99fc
Your Name <obor528622@gmail.com>
2022-02-08 11:47:57 +0700

    git

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('category/<str:slug>/', PostsByCategory.as_view(), name='category'),
     path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
     path('post/<str:slug>/', GetPost.as_view(), name='post'),
-
+    path('search/', Search.as_view(), name='search'),
+    
 ]
